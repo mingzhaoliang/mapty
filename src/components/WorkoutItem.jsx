8f34b4f851bf7e1cba221cd892ff19c96b5c7f5a
mingzhaoliang <timothymc.leung@gmail.com>
2024-02-24 09:51:48 +1100

    Update sidebar and workout item layout

--- src/components/WorkoutItem.jsx
+++ src/components/WorkoutItem.jsx
@@ -5,7 +5,7 @@ const Stats = ({ icon, value, unit }) => {
         <div className="flex items-baseline">
             <span className="mr-1 h-1">{icon}</span>
             <span className="mr-1 text-sm">{Math.round((+ value + Number.EPSILON) * 100) / 100}</span>
-            <span className="uppercase text-xs text-slate-300 font-semibold">{unit}</span>
+            <span className="uppercase text-[0.6rem] xl:text-xs text-slate-300 font-semibold">{unit}</span>
         </div>
     )
 }
@@ -24,12 +24,12 @@ export default function WorkoutItem({ type, distance, duration, pace, cadence, e
 
     return (
         <li
-            className={`relative max-h-24 bg-[#42484d] rounded cursor-pointer flex border-l-4 transition-all ${type === "running" ? "border-emerald-400" : "border-amber-400"}`}
+            className={`relative max-h-28 bg-[#42484d] rounded cursor-pointer border-l-4 transition-all ${type === "running" ? "border-emerald-400" : "border-amber-400"}`}
             data-id="1234567890"
             onMouseOver={mouseOverHandler}
             onMouseOut={mouseOutHandler}
         >
-            <div className="p-4 grid grid-cols-4-auto gap-2 text-white">
+            <div className="p-4 grid grid-cols-2 lg:grid-cols-4-auto gap-2 text-white overflow-scroll">
                 <h2 className="col-span-full font-semibold">{type === "running" ? "Running" : "Cycling"} on {date}</h2>
                 <Stats icon={type === "running" ? "🏃‍♂️" : "🚴‍♀️"} value={distance} unit="km" />
                 <Stats icon="⏱" value={duration} unit="min" />
@@ -37,7 +37,7 @@ export default function WorkoutItem({ type, distance, duration, pace, cadence, e
                 <Stats icon={type === "running" ? "🦶🏼" : "⛰"} value={cadence || elev_gain} unit={type === "running" ? "spm" : "m"} />
             </div>
             <div
-                className="absolute top-0 right-0 flex flex-col max-w-8 transition-all"
+                className="absolute top-0 right-0 flex flex-col max-w-8 transition-all h-full"
                 style={
                     isHovered
                         ? { transform: "translateX(0)" }
@@ -48,16 +48,12 @@ export default function WorkoutItem({ type, distance, duration, pace, cadence, e
                         }
                 }
             >
-                <div className="p-2 rounded-tr bg-rose-500 hover:bg-rose-600 transition-all">
-                    <button>
-                        <img src="/icons/edit.png" alt="Edit" draggable="false" className="w-4" />
-                    </button>
-                </div>
-                <div className="p-2 rounded-br bg-slate-300 hover:bg-slate-400 transition-all">
-                    <button>
-                        <img src="/icons/bin.png" alt="Delete" draggable="false" className="w-4" />
-                    </button>
-                </div>
+                <button className="p-2 rounded-tr bg-rose-500 hover:bg-rose-600 transition-all h-1/2">
+                    <img src="/icons/edit.png" alt="Edit" draggable="false" className="w-4 m-auto" />
+                </button>
+                <button className="p-2 rounded-br bg-slate-300 hover:bg-slate-400 transition-all h-1/2">
+                    <img src="/icons/bin.png" alt="Delete" draggable="false" className="w-4 m-auto" />
+                </button>
             </div>
         </li>
     )
